1893a67e1d87777a281f38efafc9a580eff0e468
Matthias Wettstein <11014911+wrangel@users.noreply.github.com>
2020-05-07 09:23:10 +0200

    Simplify Main

--- src/main/scala/ch/wrangel/toolbox/Main.scala
+++ src/main/scala/ch/wrangel/toolbox/Main.scala
@@ -7,7 +7,7 @@ import scala.util.Try
 /** Command line tool to synchronize photo file names, exif and mac timestamps
  *
  * Example arguments:
- * <PathToFiles> file false
+ * <PathToFiles> file
  * <PathToFiles> exif true
  * <PathToFiles> validate
  *
@@ -16,7 +16,7 @@ import scala.util.Try
  *      a) To apply a valid DateTimeOriginal / CreateDate to file name,
  *      mac timestamps, and the rest of the exif timestamps, OR
  *      B) To detect potentially valid exif dates outside DateTimeOriginal / CreateDate, ask for user's choice,
- *      and apply choice (if any) to file name, mac timestamps, and the rest of the exif timestamps
+ *      and apply choice (if any) to file name, mac timestamps, and the rest of the exif timestamps.
  * - 2) file
  *      To detect a valid timestamp in the file name and apply them to file name,
  *      mac timestamps, and exif timestamps.
@@ -24,23 +24,20 @@ import scala.util.Try
  *      timestamps available for neither exif nor mac, then add the date / timestamp to your photo's name.
  *      The procedure takes it from there
  * - 3) validate
- *      To check if the timestamp in file name and DateTimeOriginal / Create Date coincide. Move to sub folder
- *      "_unsuccessful" otherwise
+ *      To check if the timestamp in file name and DateTimeOriginal / Create Date coincide. Move to a sub folder
+ *      otherwise
  *
  * TODO video
- * Establish which create dates are used by photo programs - adapt "Validate" and create consistent framework
- * AVI (convert with ffmpeg, since exiftool cannot write tags)
- * MiscUtilities.convertVideo(directory) a) as part of a use case, b) extended to other conversions
+ * - Establish which create dates are used by photo programs - adapt "Validate" and create consistent framework
+ * - MiscUtilities.convertVideo(directory) a) as part of a use case, b) extended to AVI
  */
-object Main {
+object Main extends App {
 
-  def main(args: Array[String]): Unit = {
-    MiscUtilities.checkForZeroByteLengthFiles(args.head)
-    UseCaseFactory(args(1)).run(
-      args.head,
-      Try { args(2).toBoolean }
-        .getOrElse(false)
-    )
-  }
+  MiscUtilities.checkForZeroByteLengthFiles(args.head)
+  UseCaseFactory(args(1)).run(
+    args.head,
+    Try { args(2).toBoolean }
+      .getOrElse(false)
+  )
 
 }
